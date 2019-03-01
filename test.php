886af5d534f2f4eeda92fe4ec4be4bbfd0d21b41
Alyona Kovalyova <alyona@banka.space>
2019-03-01 12:29:58 +0300

    Add control of the possibility of tests passing

--- test.php
+++ test.php
@@ -21,59 +21,73 @@ require_once 'header.php.inc';
     print_header('course');
     ?>
     <div class="site_content">
-    <?php
-    $id = $_GET['id'];
-    $is_training = isset($_GET['training']);
+        <?php
+        $id = $_GET['id'];
+        $is_training = isset($_GET['training']);
 
-    if (!$is_training && !is_logged_in()) {
-        http_response_code(401);
-        echo "Для прохождения аттестующих тестов нужно сначала <a href='index.php'>войти</a>.";
-        echo "<br>";
-        echo "Можете также <a href='test.php?training&id=$id'>пройти обучающий тест</a>.";
-        die;
-    }
-
-    $test = \tests\get_test_info($id);
-    $questions = \tests\get_test_questions($id, $is_training);
-
-    ?>
-
-    <?php if ($is_training) { ?>
-        <h1>Обучающий тест №&nbsp;<?php echo $test['ordinal'] ?></h1>
-    <?php } else { ?>
-        <h1>Аттестующий тест №&nbsp;<?php echo $test['ordinal'] ?></h1>
-    <?php } ?>
-
-    <form action="check_test.php" method="post" class="test">
-        <input type="hidden" name="test_id" value="<?php echo $test['id'] ?>"/>
-        <?php foreach ($questions as $question) { ?>
-            <section class="question">
-                <input name="question<?php echo $question['ordinal'] ?>" type="hidden" value="<?php echo $question['id'] ?>">
-                <p class="question ordinal">
-                    Вопрос №&nbsp;<?php echo $question['ordinal']; ?>
-                </p>
-                <p class="question text">
-                    <?php echo $question['text_html']; ?>
-                </p>
-                <?php if ($question['options'] != null) { ?>
-                    <?php foreach (str_getcsv($question['options']) as $option) { ?>
-                        <label>
-                            <input name="answer<?php echo $question['ordinal'] ?>" type="radio"
-                                   value="<?php echo htmlspecialchars($option) ?>" required>
-                            <?php echo $option; ?>
-                        </label><br>
-                    <?php } ?>
-                <?php } else { ?>
-                    <label>
-                        Ответ:
-                        <input class="test answer" name="answer<?php echo $question['ordinal'] ?>" type="text" required>
-                    </label>
-                <?php } ?>
-            </section>
-            <hr>
+        if (!$is_training && !is_logged_in()) {
+            http_response_code(401);
+            echo "Для прохождения аттестующих тестов нужно сначала <a href='index.php'>войти</a>.";
+            echo "<br>";
+            echo "Можете также <a href='test.php?training&id=$id'>пройти обучающий тест</a>.";
+            die;
+        }
+        $test = \tests\get_test_info($id);
+        $questions = \tests\get_test_questions($id, $is_training);
+        ?>
+        <?php if ($is_training) { ?>
+            <h1>Обучающий тест №&nbsp;<?php echo $test['ordinal'] ?></h1>
         <?php } ?>
-        <button type="submit">Готово</button>
-    </form>
+        <?php
+        $possible_attempts = \tests\get_max_possible_attempts($test['id'], \tests\get_teacher_id_by_student(get_user_id()));
+        $user_attempts = \tests\get_used_attempts($test['id'], get_user_id());
+        ?>
+        <?php if (
+            \tests\can_user_do_test(get_user_id(), $test['id'])
+            || $is_training
+            || $_SESSION['user']['is_teacher']) { ?>
+            <?php if (!$is_training) { ?>
+                <h1>Аттестующий тест №&nbsp;<?php echo $test['ordinal'] ?></h1>
+            <?php } ?>
+
+            <form action="check_test.php" method="post" class="test">
+                <input type="hidden" name="is_training" value="<?php echo $is_training ?>"/>
+                <input type="hidden" name="test_id" value="<?php echo $test['id'] ?>"/>
+                <?php foreach ($questions as $question) { ?>
+                    <section class="question">
+                        <input name="question<?php echo $question['ordinal'] ?>" type="hidden"
+                               value="<?php echo $question['id'] ?>">
+                        <p class="question ordinal">
+                            Вопрос №&nbsp;<?php echo $question['ordinal']; ?>
+                        </p>
+                        <p class="question text">
+                            <?php echo $question['text_html']; ?>
+                        </p>
+                        <?php if ($question['options'] != null) { ?>
+                            <?php foreach (str_getcsv($question['options']) as $option) { ?>
+                                <label>
+                                    <input name="answer<?php echo $question['ordinal'] ?>" type="radio"
+                                           value="<?php echo htmlspecialchars($option) ?>" required>
+                                    <?php echo $option; ?>
+                                </label><br>
+                            <?php } ?>
+                        <?php } else { ?>
+                            <label>
+                                Ответ:
+                                <input class="test answer" name="answer<?php echo $question['ordinal'] ?>" type="text"
+                                       required>
+                            </label>
+                        <?php } ?>
+                    </section>
+                    <hr>
+                <?php } ?>
+                <button type="submit">Готово</button>
+            </form>
+
+        <?php } else { ?>
+            <p>Вы больше не можете пройти данный тест</p>
+        <?php } ?>
+
     </div>
 
     <footer>
